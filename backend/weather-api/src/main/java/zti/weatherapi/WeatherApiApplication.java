e2a43d0dde386dbf179a981f8fdedbe5e1566e65
unlvy <unlv13y@gmail.com>
2023-06-04 14:41:23 +0200

    swagger and checkstyle

--- backend/weather-api/src/main/java/zti/weatherapi/WeatherApiApplication.java
+++ backend/weather-api/src/main/java/zti/weatherapi/WeatherApiApplication.java
@@ -3,11 +3,12 @@ package zti.weatherapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/** Weather Api Application class. */
 @SpringBootApplication
 public class WeatherApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WeatherApiApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(WeatherApiApplication.class, args);
+  }
 
 }
